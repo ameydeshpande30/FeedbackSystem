9b9c1bef830b24b7948bb3e0d1ad27c6e2763b99
Amey Deshpande <ameydeshpande30@gmail.com>
2018-10-12 13:18:03 +0530

    FInal Changes to Login

--- src/main/java/asdsoft/DataBase.java
+++ src/main/java/asdsoft/DataBase.java
@@ -1,20 +1,23 @@
 package asdsoft;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.mysql.cj.protocol.Resultset;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.*;
+
+import static asdsoft.ApiToken.createJsonWebToken;
 
 public class DataBase {
 
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://db4free.net/sparkdatabase";
+    static final String DB_URL = "jdbc:mysql://db4free.net/sdl_project";
 
     //  Database credentials
-    static final String USER = "sparkdb";
-    static final String PASS = "Amey1234";
-    Connection conn = null;
+    static final String USER = "iamroot";
+    static final String PASS = "sdl_project";
+    static Connection conn = null;
     Statement stmt = null;
     DataBase(){
         try {
@@ -24,9 +27,41 @@ public class DataBase {
             e.printStackTrace();
         }
     }
+    public LoginData check(String uname, String pass){
+        LoginData ld = new LoginData();
+        try {
+            String Sql = String.format("SELECT * FROM `employee` WHERE username = '%s' ;", uname);
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(Sql);
+
+            while (rs.next()){
+            if(PassHash.validatePassword(pass,rs.getString("password"))){
+                ld.setUsername(rs.getString("username"));
+                ld.setFirst_name(rs.getString("first_name"));
+                ld.setLast_name(rs.getString("last_name"));
+                ld.setContact(rs.getString("contact"));
+                ld.setEmail(rs.getString("email"));
+                ld.setRating(rs.getInt("rating"));
+                ld.setToken(createJsonWebToken(ld.getUsername(),(long)3));
+                return ld;
+            }
+            ld.setToken("-1");
+                return ld;}
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        ld.setToken("-2");
+        return ld;
+    }
     public void addUser(String uname, String pass){
         try {
-            String sql = String.format("INSERT into user_table (uname, pass, is_admin) VALUES ('%s', '%s', %b);",uname, pass, false);
+            String sql = String.format("INSERT INTO `employee`(`username`, `password`, `first_name`, `last_name`, `dob`, `salary`, `contact`, `is_admin`, `rating`) VALUES ('%s', '%s' , '%s', '%s', '%s', '%s', '%s', %d, %d)", uname,
+                    PassHash.createHash(pass), "Amey", "Deshpande", "1998-04-30", 50000, "917588758032", 1, 5);
             System.out.println(sql);
             stmt = conn.createStatement();
             stmt.executeUpdate(sql);
@@ -38,5 +73,8 @@ public class DataBase {
     @Override
     protected void finalize() throws Throwable {
         conn.close();
+    }
+    public boolean Check(){
+        return true;
     }
 }
--- src/main/java/asdsoft/Main.java
+++ src/main/java/asdsoft/Main.java
@@ -1,5 +1,7 @@
 package asdsoft;
 import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.sun.corba.se.spi.ior.ObjectId;
@@ -27,12 +29,47 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         port(8000);
+        DataBase db = new DataBase();
         System.out.println("Now the output is redirected!");
-        get("/", (req, res) -> (createJsonWebToken("asd",(long)3)) );
-        post("/token", (req,res) -> {
+        get("/", (req, res) -> {
+            res.type("application/json");
+            LoginData ld = db.check("root","root1");
+
+            Gson gson = new GsonBuilder().create();
+            return gson.toJson(ld);
+            //System.out.println(PassHash.validatePassword("root","1000:bd417ce87a0050284e415254c0738a9fe1fbd9c543b770ab:6217d95786d75bd13df8b53502477c431804165d00a610b6"));
+            //return createJsonWebToken("asd",(long)3);
+        } );
+        get("/profile",(req, res) -> {
+            res.type("application/json");
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonObject out = new JsonObject();
+            out.addProperty("id", 1);
+            out.addProperty("fname", "Amey");
+            out.addProperty("lname", "Deshpande");
+            out.addProperty("vno", "MH09-AS-1234");
+            out.addProperty("phone","+91-7588758032");
+            out.addProperty("service_id", 2);
+            out.addProperty("D_Service", "2018-05-20");
+            out.addProperty("N_Service","2018-05-20");
+            out.addProperty("kms",3000);
+            return gson.toJson(out);
+        });
+        post("/login", (req,res) -> {
             String uname = req.queryParams("uname");
             String passwd = req.queryParams("pass");
-            DataBase db = new DataBase();
+            System.out.println(uname);
+            System.out.println(passwd);
+            LoginData ld = db.check(uname,passwd);
+            Gson gson = new GsonBuilder().create();
+            return gson.toJson(ld);
+
+        });
+        post("/token", (req,res) -> {
+            res.type("application/json");
+            String uname = req.queryParams("uname");
+            String passwd = req.queryParams("pass");
+
             db.addUser(uname,passwd);
             JsonArray array = new JsonArray();
             JsonObject person = new JsonObject();
@@ -41,6 +78,11 @@ public class Main {
             person.addProperty("token", createJsonWebToken(uname, (long) 1));
             array.add(person);
             return array.toString();
+        });
+        post("/getCalls", (req,res) -> {
+            String token = req.queryParams("token");
+            String  asd = req.queryParams("pass");
+            return "asd";
         });
     }
 
